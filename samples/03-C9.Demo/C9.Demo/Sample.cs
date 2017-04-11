24c7a5ae57f35747ffe7f9981ac603e318cfd790
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2017-04-11 10:53:54 -0700

    adding third template

---
+++ samples/03-C9.Demo/C9.Demo/Sample.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C9.Demo
+{
+    public static class Sample
+    {
+        public static string GetName()
+        {
+            return "Channel 9 demo";
+        }
+    }
+}
